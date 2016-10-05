81b2c662193201296c64e020bb1beb181fa2869e
Lukas Hechenberger <lukas@lukashechenberger.local>
2016-10-05 07:31:36 +0200

    Use ES6 in test server

--- test/server.js
+++ test/server.js
@@ -1,10 +1,11 @@
-const WebSocketServer = require('ws').Server;
+import { Server as WebSocketServer } from 'uws';
+
 const wss = new WebSocketServer({ port: 9000 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', ws => {
   console.log('Client connected');
 
-  ws.on('message', function incoming(message) {
+  ws.on('message', message => {
     ws.send(message);
     console.log('received: %s', message);
   });
